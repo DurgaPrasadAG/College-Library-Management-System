b5983a89fa6499227a4990496b915d80626aa05e
DurgaPrasadAG <68811536+DurgaPrasadAG@users.noreply.github.com>
2020-12-24 23:05:50 +0530

    Add files via upload

---
+++ webpage/js/div_click.js
@@ -0,0 +1,7 @@
+$("#view-ppt").click(function(){
+    window.location.href='webpage/view_presentation.html';
+});
+
+$("#view-docs").click(function(){
+    window.location.href='webpage/read_documentation.html';
+});
