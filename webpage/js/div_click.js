287393a002f91d90fe1306bc400fa91a5f746729
DurgaPrasadAG <68811536+DurgaPrasadAG@users.noreply.github.com>
2020-12-25 20:09:02 +0530

    Update div_click.js

--- webpage/js/div_click.js
+++ webpage/js/div_click.js
@@ -1,11 +1,19 @@
+// Link to open play presentation html page
 $("#play-ppt").click(function(){
-    window.location.href='webpage/view_presentation.html';
+    window.location.href='webpage/play_presentation.html';
 });
 
+// Link to open read documentation html page
 $("#read-docs").click(function(){
     window.location.href='webpage/read_documentation.html';
 });
 
+// Link to view Source code in github
 $("#view-sc").click(function(){
     window.location.href='https://github.com/DurgaPrasadAG/College-Library-Management-System';
+});
+
+// Link to Download the project from github
+$("#dload-project").click(function(){
+    window.location.href='https://github.com/DurgaPrasadAG/College-Library-Management-System/archive/main.zip';
 });
